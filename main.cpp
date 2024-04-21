0ce9e5faba0bae3c479c30a887ff0536856c4024
Marcel2201020012 <marcelaja314@gmail.com>
2024-04-21 07:11:49 +0700

    added gelas

--- main.cpp
+++ main.cpp
@@ -12,8 +12,134 @@
 const int windowWidth = 800;
 const int windowHeight = 800;
 
-const int jumlahObjek = 3;
-const int jumlahIndices = 3;
+const int jumlahObjek = 4;
+const int jumlahIndices = 4;
+
+//properti Gelas
+const int jumlahIrisan = 50;
+const float jariJari = 0.1f;
+const float tinggiGelas = 0.2f;
+
+GLfloat permukaanMeja[] =
+{
+	// Koordinat simpul		// Warna
+	// Front face
+	-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 0
+	 0.5f, -0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 1
+	 0.5f,  0.1f,  0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 2
+	-0.5f,  0.1f,  0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 3
+
+	// Back face
+	-0.5f, -0.1f, -0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 4
+	 0.5f, -0.1f, -0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 5
+	 0.5f,  0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 6
+	-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 7
+
+	// Top face
+	-0.5f,  0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 8
+	 0.5f,  0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 9
+	 0.5f,  0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 10
+	-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 11
+
+	// Bottom face
+	-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 12
+	 0.5f, -0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 13
+	 0.5f, -0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 14
+	-0.5f, -0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 15
+
+	//left face
+	-0.5f, -0.1f, -0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 16
+	-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kanan bawah 17
+	-0.5f,  0.1f,  0.5f,	1.0f, 1.0f, 1.0f, //kanan atas 18
+	-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 19
+
+	//right face
+	0.5f, -0.1f, -0.5f,		1.0f, 0.0f, 0.0f, //kiri bawah 20
+	0.5f, -0.1f,  0.5f,		1.0f, 0.0f, 0.0f, //kanan bawah 21
+	0.5f,  0.1f,  0.5f,		1.0f, 1.0f, 1.0f, //kanan atas 22
+	0.5f,  0.1f, -0.5f,		1.0f, 1.0f, 1.0f //kiri atas 23
+};
+
+GLfloat kakiMeja[] =
+{
+	//front face
+	-0.5f, -0.5f,  0.5f,	1.0f, 0.0f, 0.0f,  //0
+	-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //1
+	-0.3f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //2
+	-0.5f, -0.1f,  0.5f,	1.0f, 1.0f, 1.0f,  //3
+
+	//back face						  
+	-0.5f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //4
+	-0.3f, -0.5f,  0.3f,	0.0f, 1.0f, 0.0f,  //5
+	-0.3f, -0.1f,  0.3f,	0.0f, 0.0f, 1.0f,  //6
+	-0.5f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f,  //7
+
+	//bottom face						  
+	-0.5f, -0.5f,  0.5f,	1.0f, 0.0f, 0.0f,  //8
+	-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //9
+	-0.3f, -0.5f,  0.3f,	0.0f, 0.0f, 1.0f,  //10
+	-0.5f, -0.5f,  0.3f,	1.0f, 1.0f, 1.0f,  //11
+
+	//left face
+	-0.5f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //12
+	-0.5f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //13
+	-0.5f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //14
+	-0.5f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f,   //15
+
+	//left face
+	-0.3f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //16
+	-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //17
+	-0.3f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //18
+	-0.3f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f   //19
+};
+
+GLfloat taplakMeja[] =
+{
+	// Koordinat simpul		// Warna
+	// Top face
+	-0.5f,  0.1001f,  0.2f,		1.0f, 1.0f, 1.0f, //kiri bawah 0
+	 0.5f,  0.1001f,  0.2f,		1.0f, 1.0f, 1.0f, //kanan bawah 1
+	 0.5f,  0.1001f,  -0.2f,	1.0f, 1.0f, 1.0f, //kanan atas 2
+	-0.5f,  0.1001f,  -0.2f,	1.0f, 1.0f, 1.0f,  //kiri atas 3
+
+	//right face
+	0.50015f, -0.25f, 0.2f,		1.0f, 1.0f, 1.0f,	//kanan bawah depan 4
+	0.50015f, -0.25f, -0.2f,	1.0f, 1.0f, 1.0f,	//kanan bawah belakang 5
+
+	//left face
+	-0.50015f, -0.25f, 0.2f,	1.0f, 1.0f, 1.0f,	//kiri bawah depan 6
+	-0.50015f, -0.25f, -0.2f,	1.0f, 1.0f, 1.0f,	//kiri bawah belakang 7
+};
+
+GLfloat gelas[50 * 6 * 3];
+
+GLuint permukaanMejaIndices[] =
+{
+	0, 1, 2, 2, 3, 0,		// Front face
+	4, 5, 6, 6, 7, 4,		// Back face
+	8, 9, 10, 10, 11, 8,	// top face
+	12, 13, 14, 14, 15, 12,	// bottom face
+	16, 17, 18, 18, 19, 16,// left face
+	20, 21, 22, 22, 23, 20	// right face
+};
+
+GLuint kakiMejaIndices[] =
+{
+	0, 1, 2, 2, 3, 0,		// Front face
+	4, 5, 6, 6, 7, 4,		// back face
+	8, 9, 10, 10, 11, 8,	// bottom face
+	12, 13, 14, 14, 15, 12,		// left face
+	16, 17, 18, 18, 19, 16		// right face
+};
+
+GLuint taplakMejaIndices[] =
+{
+	0, 1, 2, 2, 3, 0,
+	1, 4, 5, 5, 2, 1,
+	0, 6, 7, 7, 3, 0
+};
+
+GLuint gelasIndices[50 * 3 * 2 + 50];
 
 void binding(GLuint& VAO, GLuint& VBO, GLuint& EBO, GLsizei vboSize ,GLfloat* vboName, GLsizei eboSize,GLuint* eboName)
 {
@@ -55,122 +181,49 @@ int main()
 
 	Shader shaderProgram("default.vert", "default.frag");
 
-	GLfloat permukaanMeja[] =
-	{
-		// Koordinat simpul		// Warna
-		// Front face
-		-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 0
-		 0.5f, -0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 1
-		 0.5f,  0.1f,  0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 2
-		-0.5f,  0.1f,  0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 3
+	for (int i = 0; i <= jumlahIrisan; ++i) {
+		float sudut = 2 * 3.14159265359 / jumlahIrisan;
+		float x = jariJari * cos(i * sudut);
+		float z = jariJari * sin(i * sudut);
 
-		// Back face
-		-0.5f, -0.1f, -0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 4
-		 0.5f, -0.1f, -0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 5
-		 0.5f,  0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 6
-		-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 7
+		// Bottom vertex
+		gelas[i * 6 * 2] = x / 2;
+		gelas[i * 6 * 2 + 1] = -tinggiGelas / 2.0f;
+		gelas[i * 6 * 2 + 2] = z / 2;
+		gelas[i * 6 * 2 + 3] = 1.0f; // Red color
+		gelas[i * 6 * 2 + 4] = 0.0f; // Green color
+		gelas[i * 6 * 2 + 5] = 0.0f; // Blue color
 
-		// Top face
-		-0.5f,  0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 8
-		 0.5f,  0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 9
-		 0.5f,  0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 10
-		-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 11
+		//alas vertex
+		gelas[i * 6 + 600] = x / 2;
+		gelas[i * 6 + 601] = -tinggiGelas / 2.0f;
+		gelas[i * 6 + 602] = z / 2;
+		gelas[i * 6 + 603] = 0.4f; // Red color
+		gelas[i * 6 + 604] = 0.2f; // Green color
+		gelas[i * 6 + 605] = 0.5f; // Blue color
+		
+		// Top vertex
+		gelas[i * 6 * 2 + 6] = x;
+		gelas[i * 6 * 2 + 7] = tinggiGelas / 2.0f;
+		gelas[i * 6 * 2 + 8] = z;
+		gelas[i * 6 * 2 + 9] = 1.0f; // Red color
+		gelas[i * 6 * 2 + 10] = 0.0f; // Green color
+		gelas[i * 6 * 2 + 11] = 0.0f; // Blue color
+	}
 
-		// Bottom face
-		-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 12
-		 0.5f, -0.1f,  0.5f,	0.0f, 1.0f, 0.0f, //kanan bawah 13
-		 0.5f, -0.1f, -0.5f,	0.0f, 0.0f, 1.0f, //kanan atas 14
-		-0.5f, -0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 15
+	for (int i = 0; i < jumlahIrisan; ++i) {
+		// sisi gelas indices
+		gelasIndices[i * 6] = i * 2;
+		gelasIndices[i * 6 + 1] = (i + 1) * 2;
+		gelasIndices[i * 6 + 2] = (i + 1) * 2 + 1;
 
-		//left face
-		-0.5f, -0.1f, -0.5f,	1.0f, 0.0f, 0.0f, //kiri bawah 16
-		-0.5f, -0.1f,  0.5f,	1.0f, 0.0f, 0.0f, //kanan bawah 17
-		-0.5f,  0.1f,  0.5f,	1.0f, 1.0f, 1.0f, //kanan atas 18
-		-0.5f,  0.1f, -0.5f,	1.0f, 1.0f, 1.0f, //kiri atas 19
+		gelasIndices[i * 6 + 3] = (i + 1) * 2 + 1;
+		gelasIndices[i * 6 + 4] = i * 2 + 1;
+		gelasIndices[i * 6 + 5] = i * 2;
 
-		//right face
-		0.5f, -0.1f, -0.5f,		1.0f, 0.0f, 0.0f, //kiri bawah 20
-		0.5f, -0.1f,  0.5f,		1.0f, 0.0f, 0.0f, //kanan bawah 21
-		0.5f,  0.1f,  0.5f,		1.0f, 1.0f, 1.0f, //kanan atas 22
-		0.5f,  0.1f, -0.5f,		1.0f, 1.0f, 1.0f //kiri atas 23
-	};
-
-	GLfloat kakiMeja[] =
-	{
-		//front face
-		-0.5f, -0.5f,  0.5f,	1.0f, 0.0f, 0.0f,  //0
-		-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //1
-		-0.3f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //2
-		-0.5f, -0.1f,  0.5f,	1.0f, 1.0f, 1.0f,  //3
-													  
-		//back face						  
-		-0.5f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //4
-		-0.3f, -0.5f,  0.3f,	0.0f, 1.0f, 0.0f,  //5
-		-0.3f, -0.1f,  0.3f,	0.0f, 0.0f, 1.0f,  //6
-		-0.5f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f,  //7
-													  
-		//bottom face						  
-		-0.5f, -0.5f,  0.5f,	1.0f, 0.0f, 0.0f,  //8
-		-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //9
-		-0.3f, -0.5f,  0.3f,	0.0f, 0.0f, 1.0f,  //10
-		-0.5f, -0.5f,  0.3f,	1.0f, 1.0f, 1.0f,  //11
-
-		//left face
-		-0.5f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //12
-		-0.5f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //13
-		-0.5f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //14
-		-0.5f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f,   //15
-
-		//left face
-		-0.3f, -0.5f,  0.3f,	1.0f, 0.0f, 0.0f,  //16
-		-0.3f, -0.5f,  0.5f,	0.0f, 1.0f, 0.0f,  //17
-		-0.3f, -0.1f,  0.5f,	0.0f, 0.0f, 1.0f,  //18
-		-0.3f, -0.1f,  0.3f,	1.0f, 1.0f, 1.0f   //19
-	};												  
-
-	GLfloat taplakMeja[] =
-	{
-		// Koordinat simpul		// Warna
-		// Top face
-		-0.5f,  0.1001f,  0.2f,		1.0f, 1.0f, 1.0f, //kiri bawah 0
-		 0.5f,  0.1001f,  0.2f,		1.0f, 1.0f, 1.0f, //kanan bawah 1
-		 0.5f,  0.1001f,  -0.2f,	1.0f, 1.0f, 1.0f, //kanan atas 2
-		-0.5f,  0.1001f,  -0.2f,	1.0f, 1.0f, 1.0f,  //kiri atas 3
-
-		//right face
-		0.50015f, -0.25f, 0.2f,		1.0f, 1.0f, 1.0f,	//kanan bawah depan 4
-		0.50015f, -0.25f, -0.2f,	1.0f, 1.0f, 1.0f,	//kanan bawah belakang 5
-
-		//left face
-		-0.50015f, -0.25f, 0.2f,	1.0f, 1.0f, 1.0f,	//kiri bawah depan 6
-		-0.50015f, -0.25f, -0.2f,	1.0f, 1.0f, 1.0f,	//kiri bawah belakang 7
-	};
-
-	GLuint permukaanMejaIndices[] =
-	{
-		0, 1, 2, 2, 3, 0,		// Front face
-		4, 5, 6, 6, 7, 4,		// Back face
-		8, 9, 10, 10, 11, 8,	// top face
-		12, 13, 14, 14, 15, 12,	// bottom face
-		16, 17, 18, 18, 19, 16,// left face
-		20, 21, 22, 22, 23, 20	// right face
-	};
-
-	GLuint kakiMejaIndices[] =
-	{
-		0, 1, 2, 2, 3, 0,		// Front face
-		4, 5, 6, 6, 7, 4,		// back face
-		8, 9, 10, 10, 11, 8,	// bottom face
-		12, 13, 14, 14, 15, 12,		// left face
-		16, 17, 18, 18, 19, 16		// right face
-	};
-
-	GLuint taplakMejaIndices[] =
-	{
-		0, 1, 2, 2, 3, 0,
-		1, 4, 5, 5, 2, 1,
-		0, 6, 7, 7, 3, 0
-	};
+		// alas gelas indices
+		gelasIndices[i + 300] = i * 2;
+	}
 
 	GLuint VAO[jumlahObjek], VBO[jumlahObjek], EBO[jumlahIndices];
 	glGenVertexArrays(jumlahObjek, VAO);
@@ -183,6 +236,9 @@ int main()
 
 	//Taplak Meja
 	binding(VAO[2], VBO[2], EBO[2], sizeof(taplakMeja), taplakMeja, sizeof(taplakMejaIndices), taplakMejaIndices);
+	
+	//gelas
+	binding(VAO[3], VBO[3], EBO[3], sizeof(gelas), gelas, sizeof(gelasIndices), gelasIndices);
 
 	GLuint translationUniform = glGetUniformLocation(shaderProgram.ID, "translation");
 
@@ -219,6 +275,11 @@ int main()
 		glUniform3f(translationUniform, 0.0f, 0.0f, 0.0f);
 		glBindVertexArray(VAO[2]);
 		glDrawElements(GL_TRIANGLES, sizeof(taplakMejaIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+		//gelas
+		glUniform3f(translationUniform, 0.0f, 0.2001f, -0.3f);
+		glBindVertexArray(VAO[3]);
+		glDrawElements(GL_TRIANGLE_FAN, sizeof(gelas) / sizeof(int), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
